61ff67c1a46b1698aca91eec404e2e2cb5fd0cbc
manassanan2210 <panghoam.tip2210@gmail.com>
2025-10-22 16:16:30 +0700

    "feat: add restaurant database and API endpoints"

---
+++ database/restaurants.js
@@ -0,0 +1,23 @@
+const restaurants = [
+{
+id: 1,
+name: "The Never Ending Story",
+cuisine: "ฟิวชั่นไทย",
+priceRange: "$$",
+rating: 4.5,
+location: "สยามสแควร์",
+image: "images/restaurant1.jpg"
+},
+{
+id: 2,
+name: "Pasta La Vista",
+cuisine: "อิตาเลียน",
+
+priceRange: "$$$",
+rating: 4.2,
+location: "ทองหล่อ",
+image: "images/restaurant2.jpg"
+}
+];
+
+module.exports = restaurants;
